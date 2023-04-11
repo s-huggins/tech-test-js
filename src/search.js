5b930a38cb7de7767fa633071bc3eac3e761fb4d
Stuart Huggins <stuhuggins@gmail.com>
2023-04-11 16:25:16 +0100

    implement search function

--- src/search.js
+++ src/search.js
@@ -1,3 +1,17 @@
 module.exports = (data, query) => {
-  // write your code here
-}
+  let filteredResults = data;
+  Object.entries(query).forEach(([queryKey, queryValue]) => {
+    const querySegments = queryKey.split(".");
+    filteredResults = filteredResults.filter((result) => {
+      let queryLookup = result;
+      for (const segment of querySegments) {
+        if (!queryLookup) {
+          break;
+        }
+        queryLookup = queryLookup[segment];
+      }
+      return queryLookup === queryValue;
+    });
+  });
+  return filteredResults;
+};
